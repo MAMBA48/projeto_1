8364cf4226f515eb10cfa21cce27490234d75da3
MAMBA48 <oicedu48@gmail.com>
2022-05-04 00:31:46 -0300

    portal_1.0

---
+++ code.js
@@ -0,0 +1,29 @@
+let anoNasc = "1998";
+let anoAt = "2022";
+
+let idade = (anoAt - anoNasc);
+    var opa = "oi";
+    let show = "Voce nasceu em "+anoNasc+" e o ano atual for "+anoAt+" sua idade é: "+" "+idade;
+/* let text = "hello boyz";
+
+    console.log(text);
+
+function btLogin() {
+    var done = 0;
+    var user = document.user.username.value;
+    user = user.toLowerCase();
+    var password = document.user.password.value;
+    password = password.toLowerCase();
+
+    if (user == "icedu" && password == "1234") {
+        window.location = "admin.html";
+        done = 1;
+    }
+    if (done == 0) {
+        alert("invalid access!");
+    }
+}
+var iKeyCode;   
+iKeyCode = objEvent.keyCode;
+if(iKeyCode == 13) return false;
+return true; */
---
+++ table.js
@@ -0,0 +1,10 @@
+var usu="ICEDU";
+let name="Eduardo Nascimento de Campos";
+
+let menu = document.getElementById('menu');
+let li = document.createElement('li');
+    li.textContent = "Suave Men?";
+    menu.appendChild(li);
+        console.log(menu.innerHTML);
+
+ 
